4ffc6119389ce05c756a5dbcb0fb7f47cc77fb9f
Hiz Jamali <hiz.jamali@csiro.au>
2018-08-29 15:23:11 +1000

    added file path to script

--- scripts/rename_sensor_ids.R
+++ scripts/rename_sensor_ids.R
@@ -1,3 +1,9 @@
 # created by Hiz jamali
 # created on 29/08/2018
 # To rename sensor IDs in agronomic datasheet
+
+library(tidyverse)
+library(readr)
+
+df <- read_csv("c://users/jam19e/dataschool/2015_canopy_temperature_cotton/data/ct_database_ds.csv")
+
