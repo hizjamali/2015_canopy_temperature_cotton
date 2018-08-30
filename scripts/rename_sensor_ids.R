c18f73ef0a83e89751bde53e3f8515bebdc268cd
Chris Nunn <christopher.nunn@csiro.au>
2018-08-30 14:33:27 +1000

    Updated R script to now rename sensor files based on the UID

--- scripts/rename_sensor_ids.R
+++ scripts/rename_sensor_ids.R
@@ -5,5 +5,20 @@
 library(tidyverse)
 library(readr)
 
-df <- read_csv("c://users/jam19e/dataschool/2015_canopy_temperature_cotton/data/ct_database_ds.csv")
+df <- read_csv("P:/ChrisN/R scripts/2015_canopy_temperature_cotton/data/ct_database_ds.csv")
 
+sensors <- list.files("P:/ChrisN/R scripts/2015_canopy_temperature_cotton/data", full.names = T,pattern = "s*.csv", recursive = T) %>% 
+  as.tibble() %>% 
+  filter(!str_detect(value, pattern = "database")) %>% 
+  mutate(path = dirname(value),
+         sensor = basename(value)) %>% 
+  
+  
+  separate(col = value, sep = "/", into = c("path","year", "location","experiment","sensor"), extra = "merge",fill = "right") %>% 
+  mutate(sensor = str_remove(sensor, pattern = "^s")) %>% 
+  unite(col = "sensor_uid", year, location, sensor, sep = "_", remove = F) %>% 
+  unite(col = "new_path", path, sensor_uid, sep = "/", remove = F)
+
+file.rename(sensors$value, sensors$new_path)
+
+dirname("P:/ChrisN/R scripts/2015_canopy_temperature_cotton/data/ct_database_ds.csv")
