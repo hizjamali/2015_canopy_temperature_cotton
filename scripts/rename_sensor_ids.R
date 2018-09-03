9bafbd57c6b6060b3387e0d5da8e498ac282d596
Chris Nunn <christopher.nunn@csiro.au>
2018-09-03 15:57:54 +1000

    tidying code and starting comparison between metadata and files

--- scripts/rename_sensor_ids.R
+++ scripts/rename_sensor_ids.R
@@ -2,25 +2,30 @@
 # created on 29/08/2018
 # To rename sensor IDs in agronomic datasheet
 
+
+rm(list=ls(all=TRUE))##clear workspace
+print(ls())##check output is zero to confirm clear workspace
+
+
 library(tidyverse)
 library(readr)
 
 df <- read_csv("P:/ChrisN/R scripts/2015_canopy_temperature_cotton/data/ct_database_ds.csv")
 
-<<<<<<< HEAD
 sensors <- list.files("P:/ChrisN/R scripts/2015_canopy_temperature_cotton/data", full.names = T,pattern = "s*.csv", recursive = T) %>% 
   as.tibble() %>% 
   filter(!str_detect(value, pattern = "database")) %>% 
   mutate(path = dirname(value),
-         sensor = basename(value)) %>% 
-  
-  
+         sensor_filename = basename(value)) %>% 
   separate(col = value, sep = "/", into = c("path","year", "location","experiment","sensor"), extra = "merge",fill = "right") %>% 
   mutate(sensor = str_remove(sensor, pattern = "^s")) %>% 
-  unite(col = "sensor_uid", year, location, sensor, sep = "_", remove = F) %>% 
+  unite(col = "sensor_uid", experiment, sensor, sep = "_", remove = F) %>% 
+  #unite(col = "sensor_uid", year, location, sensor, sep = "_", remove = F) %>% 
   unite(col = "new_path", path, sensor_uid, sep = "/", remove = F)
 
-file.copy(sensors$value, sensors$new_path)
+
+
+#file.copy(sensors$value, sensors$new_path)
 
 dirname("P:/ChrisN/R scripts/2015_canopy_temperature_cotton/data/ct_database_ds.csv")
 =======
