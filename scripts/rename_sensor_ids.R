98877451aa940603b532ec1b4227089f48d0080d
Chris Nunn <christopher.nunn@csiro.au>
2018-09-17 16:00:56 +1000

    Updated rename script with new dir structure

--- scripts/rename_sensor_ids.R
+++ scripts/rename_sensor_ids.R
@@ -8,22 +8,25 @@ print(ls())##check output is zero to confirm clear workspace
 
 library(tidyverse)
 
-setwd("P:/ChrisN/R scripts/2015_canopy_temperature_cotton/data/")
+setwd("K:/AusBIOTIC database/Definative_cT_Raw/canopy_temperature_data_to_be_uploaded_to_DAP/canopy_temperature")
 
-meta_data <- read_csv("ct_database_ds.csv")
+meta_data <- read_csv("../cotton_canopy_sensor_agronomic_data.csv")
 
-sensors <- list.files(full.names = F,pattern = "s*.csv", recursive = T) %>% 
+sensors <- list.files(full.names = F,pattern = "*.csv", recursive = T) %>% 
   as.tibble() %>% 
   filter(!str_detect(value, pattern = "database")) %>% 
   mutate(path = dirname(value),
          sensor_filename = basename(value)) %>%
-  separate(col = path, sep = "/", into = c("year","location","experiment"), remove = F, extra = "merge",fill = "right") %>%
+  separate(col = path, sep = "/", into = c("planting_year","location","experiment_name"), remove = F, extra = "merge",fill = "right") %>%
+  mutate(planting_year = as.integer(planting_year)) %>% 
+  # mutate(sensor_filename = str_replace(sensor_filename, pattern = "^.*PHEN", "PHEN")) %>% 
+  # mutate(sensor_filename = str_remove(sensor_filename, pattern = "-Object Temperature-Celsius")) %>% 
   mutate(sensor_filename = str_remove(sensor_filename, pattern = "^s")) %>% 
-  mutate(sensor = str_remove(sensor_filename, pattern = ".csv")) %>% 
-  mutate(uniq_ID = paste(year, location, sensor, sep = "_")) %>% 
-  mutate(sensor_filename_new = paste0(uniq_ID,".csv")) %>% 
-  mutate(new_dir = paste("renamed_files", year, location, experiment, sep = "/")) %>% 
-  mutate(new_path = paste(new_dir, sensor_filename_new, sep = "/"))
+  mutate(sensorID = str_remove(sensor_filename, pattern = ".csv")) #%>% 
+  # mutate(UID = paste(year, location, sensor, sep = "_")) %>% 
+  # mutate(sensor_filename_new = paste0(UID,".csv")) %>% 
+  # mutate(new_dir = paste("renamed_files", year, location, experiment, sep = "/")) %>% 
+  # mutate(new_path = paste(new_dir, sensor_filename_new, sep = "/"))
   
 
 # Check which data streams are missing from metadata table
